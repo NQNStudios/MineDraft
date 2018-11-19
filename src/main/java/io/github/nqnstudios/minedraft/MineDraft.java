10bcf3768ce3e1d460b64642931df7a7fa36f56c
Nat Quayle Nelson <natquaylenelson@gmail.com>
2018-11-19 02:02:59 -0700

    Renaming 'examplemod' to MineDraft

--- src/main/java/io/github/nqnstudios/minedraft/MineDraft.java
+++ src/main/java/io/github/nqnstudios/minedraft/MineDraft.java
@@ -1,4 +1,4 @@
-package com.example.examplemod;
+package io.github.nqnstudios.minedraft;
 
 import net.minecraft.init.Blocks;
 import net.minecraftforge.fml.common.Mod;
@@ -7,12 +7,12 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
-@Mod(modid = ExampleMod.MODID, name = ExampleMod.NAME, version = ExampleMod.VERSION)
-public class ExampleMod
+@Mod(modid = MineDraft.MODID, name = MineDraft.NAME, version = MineDraft.VERSION)
+public class MineDraft
 {
-    public static final String MODID = "examplemod";
-    public static final String NAME = "Example Mod";
-    public static final String VERSION = "1.0";
+    public static final String MODID = "minedraft";
+    public static final String NAME = "MineDraft";
+    public static final String VERSION = "0.0.1";
 
     private static Logger logger;
 
@@ -25,7 +25,6 @@ public class ExampleMod
     @EventHandler
     public void init(FMLInitializationEvent event)
     {
-        // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+        logger.info("Hello, world!");
     }
 }
